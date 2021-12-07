ab8f8ef2af11abec6ec2d7f811dd33cb260f79ff
hackexperience <banerjee1909@gmail.com>
2021-12-06 21:09:36 -0800

    New Add

--- index.py
+++ index.py
@@ -1,84 +1,83 @@
 import numpy as np
-import cv2
 import face_recognition
+import cv2
 import time
 import datetime
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
-
-TRIG = 4
-ECHO = 18
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
-
-GPIO.output(TRIG, True)
-time.sleep(0.0001)
-GPIO.output(TRIG, False)
-
-while GPIO.input(ECHO) == False:
-    start = time.time()
-while GPIO.input(ECHO) == True:
-    end = time.time()
-
-sig_time = end-start
-
-#centimeters:
-distance = sig_time / 0.000058
-
-print('Distance:{}cm'.format(distance))
-
-GPIO.cleanup()
+# import RPi.GPIO as GPIO
+#
+# GPIO.setmode(GPIO.BCM)
+#
+# TRIG = 4
+# ECHO = 18
+# GPIO.setup(TRIG, GPIO.OUT)
+# GPIO.setup(ECHO, GPIO.IN)
+#
+# GPIO.output(TRIG, True)
+# time.sleep(0.0001)
+# GPIO.output(TRIG, False)
+#
+# while GPIO.input(ECHO) == False:
+#     start = time.time()
+# while GPIO.input(ECHO) == True:
+#     end = time.time()
+#
+# sig_time = end-start
+#
+# #centimeters:
+# distance = sig_time / 0.000058
+#
+# print('Distance:{}cm'.format(distance))
+#
+# GPIO.cleanup()
 
 cap = cv2.VideoCapture(1)
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_fullbody.xml")
+
 detection = False
 detection_stopped_time = None
 timer_started = False
-RECORD_AFTER_DETECTION = 5
-
+SECONDS_TO_RECORD_AFTER_DETECTION = 5
 
 frame_size = (int(cap.get(3)), int(cap.get(4)))
-fourcc = cv2.VideoWriter(*".mp4v")
-out = cv2.VideoWriter("Video.mp4", fourcc, 20.0, frame_size)
-
-
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 
 while True:
     _, frame = cap.read()
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    body = body_cascade.detectMultiScale(gray, 1.3, 5)
+    bodies = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    if len(faces) + len(body) > 0:
+    if len(faces) + len(bodies) > 0:
         if detection:
             timer_started = False
         else:
             detection = True
-            current_time = datetime.datetime.now().strftime("%d-%m-%y-%H-%M-%S")
-            out = cv2.VideoWriter(f"{current_time}.mp4", fourcc, 20.0, frame_size)
-            print("Started Recording")
+            current_time = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+            out = cv2.VideoWriter(
+                f"{current_time}.mp4", fourcc, 20, frame_size)
+            print("Started Recording!")
     elif detection:
         if timer_started:
-            if time.time() - detection_stopped_time >= RECORD_AFTER_DETECTION:
+            if time.time() - detection_stopped_time >= SECONDS_TO_RECORD_AFTER_DETECTION:
                 detection = False
                 timer_started = False
                 out.release()
-                print("Stop Recording")
+                print('Stop Recording!')
         else:
             timer_started = True
             detection_stopped_time = time.time()
-    out.write(frame)
-    
-    for (x, y, width, height) in faces:
-        cv2.rectangle(frame, (x,y), (x+width, y+height), (255,0,0), 3) # draw a rectangle on the image from the variable called frome, so the (x,y) statement points
-                                                                       # where x represents the top right corner and y represents the bottom left corner. The next statement
-                                                                       # (x+width, y+height) helps to form a rectangle. The next part of the code is for colour (format: Blue, Green, Red) and thickness.
-        
 
-    
+    if detection:
+        out.write(frame)
+
+    # for (x, y, width, height) in faces:
+    #    cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
+
     cv2.imshow("Camera", frame)
 
     if cv2.waitKey(1) == ord('q'):
